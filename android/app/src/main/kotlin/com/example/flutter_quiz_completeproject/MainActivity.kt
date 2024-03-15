0e95a087aa889b36f9c5c5c7a6fd36ce5f1c2aad
sachin <sachinbansal091@gmail.com>
2024-03-15 16:24:07 +0530

    First commit

---
+++ android/app/src/main/kotlin/com/example/flutter_quiz_completeproject/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_quiz_completeproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_quiz_completeproject/quiz.dart';
+
+void main() {
+  runApp(const Quiz());
+}
